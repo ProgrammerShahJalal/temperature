d7dc503129afdb4d6f52463eeeb4eba64ad67152
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-09-04 12:12:02 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
+const API_KEY = `c619eea121fbbf832bbb8eee50dcaf86`;
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
